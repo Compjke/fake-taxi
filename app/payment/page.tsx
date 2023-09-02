0a2b4d0ee0650a4f84fe90f22356eb05cc358a19
Alex Suvorov <compjkee@gmail.com>
2023-09-02 21:58:36 +0300

    s

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,28 +1,27 @@
-'use client'
+"use client";
 
-import { useContext } from 'react';
-import { SelectedCarAmountContext } from '../context/SelectedCarAmountContext';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import CheckOutForm from '@/components/Payment/CheckOutForm';
+import { useContext } from "react";
+import { SelectedCarAmountContext } from "../context/SelectedCarAmountContext";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "@/components/Payment/CheckOutForm";
 
 const Payment = () => {
-  
-   
-   const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISH_KEY!);
-   
-   const option = {
-     mode: "payment",
-     amount: 547,
-     currency: "usd",
-   };
-   return (
-     <Elements stripe={stripePromise} options={option}>
-       <div className="mx-auto max-w-xl">
-         <CheckOutForm />
-       </div>
-     </Elements>
-   );
-}
- 
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISH_KEY!);
+
+  const options = {
+    mode: "payment",
+    amount: 547,
+    currency: "usd",
+  };
+  return (
+     //@ts-ignore
+    <Elements stripe={stripePromise} options={options}>
+      <div className="mx-auto max-w-xl">
+        <CheckOutForm />
+      </div>
+    </Elements>
+  );
+};
+
 export default Payment;
